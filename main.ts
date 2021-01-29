868a67a4f66732d1d7efcf76cd340e62f16f085f
Arsen Trynko <jarkodara@gmail.com>
2021-01-29 17:49:45 +0200

    TASK 1,2,3,4

---
+++ main.ts
@@ -0,0 +1,80 @@
+
+
+//--------------TASK 1---------------------------
+
+
+// let city: string;
+// city = 'Киів';
+// city = 'Львів';
+// let adress: string = city;
+// console.log(adress);
+
+
+
+//-----------------------TASK 2--------------------------
+
+
+// let number: number = Number(prompt('Enter number'));
+// if(number != 0){
+//     console.log((number%2 ? 'Число не парне' : 'Число парне'));
+// }
+// else{
+//     console.log('Число 0');
+
+// }
+
+
+//-------------------TASK 3--------------------------
+
+
+
+// let arr: Array<number>;
+
+// max(5,-2)
+// max(5,-2,30,6)
+
+
+// function max(...numbers: Array<number>): void{
+//     numbers.sort((a,b) => a-b);
+//     console.log(numbers[numbers.length -1]);
+// }
+
+
+//----------------------TASK 4------------------------------
+
+
+// function getSqrt(number?): void {
+
+//     if (number == undefined) {
+//         console.log('Please enter a number!!!');
+
+//     }
+//     else {
+//         if (typeof number == 'number') {
+//             if (number < 0) {
+//                 console.log('Enter value higher than 0!!!');
+
+//             }
+//             else {
+//                 console.log(Math.sqrt(number));
+
+//             }
+//         }
+//         else {
+//             console.log('Must be only a number!!!');
+
+//         }
+//     }
+
+
+// }
+
+// getSqrt()
+// getSqrt('hello')
+// getSqrt(-100)
+// getSqrt(100)
+
+//--------------------------TASK 5------------------------------------
+
+
+
